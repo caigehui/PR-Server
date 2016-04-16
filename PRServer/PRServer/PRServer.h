0591d4163d64e442a0282435bd0882df81089d33
Coffee Cai <1136687@qq.com>
2016-04-16 17:13:10 +0800

    PR-Server

---
+++ PRServer/PRServer/PRServer.h
@@ -0,0 +1,19 @@
+//
+//  PRServer.h
+//  PRServer
+//
+//  Created by caigehui on 4/16/16.
+//  Copyright © 2016 caigehui. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PRServer.
+FOUNDATION_EXPORT double PRServerVersionNumber;
+
+//! Project version string for PRServer.
+FOUNDATION_EXPORT const unsigned char PRServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PRServer/PublicHeader.h>
+
+
